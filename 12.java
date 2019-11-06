35a7b52b5675fbc6a17f92523b6431ddda112f51
Tucker Carlson-Huber <tucker61761@gmail.com>
2019-11-06 10:22:25 -0600

    Fixed 12

--- 12.java
+++ 12.java
@@ -1,3 +1,4 @@
+
 /**
  * @(#)IntroToForLoops.java
  *
@@ -7,122 +8,96 @@
  */
 
 import javax.swing.JOptionPane;
-import java.util.ArrayList;
 
-class assignmentTwelve
-{
-    public static void main(String[] args)
-    {
-    	int number = GetNumber();
-    	printOddIntegerAndSum(number); //Solution to (i)
-    	factorial(number);  //Solution to (ii)
-    	divisibleByTwoOrThree(number);  //Solution to part (iii)
-    	printReverse(number);  //Solution to part (iv)
-    	primeNumbers(number);  // Solution to part (v)
+class assignmentTwelve {
+  public static void main(String[] args) {
+    int number = GetNumber();
+    printOddIntegerAndSum(number); // Solution to (i)
+    factorial(number); // Solution to (ii)
+    divisibleByTwoOrThree(number); // Solution to part (iii)
+    printReverse(number); // Solution to part (iv)
+    primeNumbers(number); // Solution to part (v)
+  }
+
+  // Gets a number from the user
+  static int GetNumber() {
+    String Str = JOptionPane.showInputDialog("Enter a positive integer value ");
+    return Integer.parseInt(Str);
+  }
+
+  // This is the solution to part (i)
+  static void printOddIntegerAndSum(int n) {
+    int total = 0;
+    for (int i = 1; i < n + 1; i += 2) {
+      System.out.println(i);
+      total += i;
     }
+    System.out.println(total);
 
+    System.out.println(); // Leaves space between the outputs
+    System.out.println();
+  }
 
-    //Gets a number from the user
-    static int GetNumber()
-    {
-    	String Str =  JOptionPane.showInputDialog("Enter a positive integer value ");
-    	return Integer.parseInt(Str);
+  // This is the solution to part (ii)
+  static void factorial(int n) {
+    long total = 1;
+    for (int i = 1; i <= n; i++) {
+      total *= i;
     }
+    System.out.println(total);
 
+    System.out.println(); // Leaves space between the outputs
+    System.out.println();
+  }
 
-
-    //This is the solution to part (i)
-    static void printOddIntegerAndSum(int n)
-    { // eww your indentations are weird
-
-      int total = 0;
-      for(int i = 1; i < n + 1; i += 2){
+  // This is the solution to part (iii)
+  static void divisibleByTwoOrThree(int n) {
+    // aha, FizzBuzz in disguise
+    for (int i = 0; i < n + 1; i++) {
+      int counter = 0;
+      if (i % 2 == 0) {
+        counter++;
+      }
+      if (i % 3 == 0) {
+        counter++;
+      }
+      if (counter == 1) {
         System.out.println(i);
-        total += i;
       }
-      System.out.println(total);
-
-    	System.out.println(); // Leaves space between the outputs
-    	System.out.println();
     }
 
+    System.out.println(); // Leaves space between the outputs
+    System.out.println();
+  }
 
-    //This is the solution to part (ii)
-    static void factorial(int n)
-    {
+  // This is a solution to part (iv)
+  static void printReverse(int n) {
 
-      long total = 1;
-      for(int i = 1; i <= n; i++){
-        total *= i;
+    for (int i = n; i > 0; i--) {
+      for (int k = 0; k < i; k++) {
+        System.out.print(i);
       }
-      System.out.println(total);
-
-    	System.out.println();   // Leaves space between the outputs
       System.out.println();
     }
 
+    System.out.println(); // Leaves space between the outputs
+    System.out.println();
 
+  }
 
-    // This is the solution to part (iii)
-    static void divisibleByTwoOrThree(int n)
-    {
-      // aha, FizzBuzz in disguise
-    	for(int i = 0; i < n + 1; i++){
-        int counter = 0;
-        if(i%2==0){
-          counter++;
-        }
-        if (i%3==0){
-          counter++;
-        }
-        if(counter==1){
-          System.out.println(i);
-        }
+  // This is a solution to part (v)
+  static void primeNumbers(int n) {
+    // This is horribly inefficient since I can't keep track of which primes I've
+    // already found but ok
+    for (int i = 0; i < n; i++) {
+      boolean prime = true;
+      for (int o = 2; o < i; o++) {
+        if (((float) i / o) % 1 == 0)
+          prime = false;
       }
-
-        System.out.println();     // Leaves space between the outputs
-        System.out.println();
+      if (prime)
+        System.out.println(i);
     }
-
-
-
-    // This is a solution to part (iv)
-    static void printReverse(int n)
-    {
-
-      for(int i = n; i > 0; i--){
-        for(int k = 0; k < i; k++){
-          System.out.print(i);
-        }
-        System.out.println();
-      }
-
-
-    	System.out.println();     // Leaves space between the outputs
-        System.out.println();
-
-    }
-
-
-    // This is a solution to part (v)
-    static void primeNumbers(int n)
-    {
-      ArrayList<Integer> primes = new ArrayList(); // the compiler doesn't like this, but it works lol
-      primes.add(2);
-      int primeCounter = 1;
-      for(int i = 3; i < n+1; i++){
-        boolean isPrime = true;
-        for(int k = 0; k < primeCounter; k++){
-          if(i%primes.get(k)==0){
-            isPrime = false;
-          }
-        }
-        if(isPrime){
-          primes.add(i);
-          System.out.println(i);
-          primeCounter++;
-        }
-      }
-    }
+  }
 
 }
