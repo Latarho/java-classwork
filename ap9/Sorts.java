6ae4f609f09f0cdf045f57b24ff907d145842d65
Tucker Carlson-Huber <tucker61761@gmail.com>
2020-03-12 10:37:26 -0500

    Finished more sorts

--- ap9/Sorts.java
+++ ap9/Sorts.java
@@ -1,16 +1,29 @@
+
 /**
  * Sorts
  */
 public class Sorts {
 
+	static final int COUNT = 10; // Number of samples to run
+	static final int LENGTH = 4096; // Length of the arrays
+
 	public static void main(String[] args) {
-		System.out.println("Running ten bubble sorts...");
-		int[] reads = new int[10];
-		int[] writes = new int[10];
-		long[] times = new long[10];
-		for (int i = 0; i < 10; i++) {
-			System.out.println("Bubble sort " + i + "...");
-			ProfiledList<Integer> result = bubble(1024);
+		// System.err.println("Generating ten 4096-element arrays...");
+		// stderr is used for logging as per unix best practices to allow users to
+		// easily pipe results to a file. (this outputs valid yaml to stdout)
+		Integer arrays[][] = new Integer[COUNT][LENGTH];
+		for (int array = 0; array < arrays.length; array++) {
+			for (int i = 0; i < arrays[array].length; i++) {
+				arrays[array][i] = (int) Math.round(Math.random() * LENGTH);
+			}
+		}
+		// System.err.println("Running bubble sorts...");
+		int[] reads = new int[COUNT];
+		int[] writes = new int[COUNT];
+		long[] times = new long[COUNT];
+		for (int i = 0; i < arrays.length; i++) {
+			// System.err.println("Bubble sort " + i + "...");
+			ProfiledList<Integer> result = bubble(arrays[i]);
 			reads[i] = result.reads;
 			writes[i] = result.writes;
 			times[i] = result.elapsed;
@@ -19,38 +32,135 @@ public class Sorts {
 		for (int i = 0; i < reads.length; i++)
 			avgReads += reads[i];
 		avgReads /= reads.length;
-		System.out.println("Average bubble read count: " + avgReads);
+		System.out.println("bubbleRead: " + avgReads);
 		float avgWrites = 0;
 		for (int i = 0; i < writes.length; i++)
 			avgWrites += writes[i];
 		avgWrites /= writes.length;
-		System.out.println("Average bubble write count: " + avgWrites);
+		System.out.println("bubbleWrite: " + avgWrites);
 		float avgTime = 0;
 		for (int i = 0; i < reads.length; i++)
 			avgTime += times[i];
 		avgTime /= times.length;
-		System.out.println("Average bubble time: " + avgTime);
+		System.out.println("bubbleCompute: " + avgTime);
+		// System.err.println("Running selection sorts...");
+		for (int i = 0; i < arrays.length; i++) {
+			// System.err.println("Selection sort " + i + "...");
+			ProfiledList<Integer> result = selection(arrays[i]);
+			reads[i] = result.reads;
+			writes[i] = result.writes;
+			times[i] = result.elapsed;
+		}
+		avgReads = 0;
+		for (int i = 0; i < reads.length; i++)
+			avgReads += reads[i];
+		avgReads /= reads.length;
+		System.out.println("selectionRead: " + avgReads);
+		avgWrites = 0;
+		for (int i = 0; i < writes.length; i++)
+			avgWrites += writes[i];
+		avgWrites /= writes.length;
+		System.out.println("selectionWrite: " + avgWrites);
+		avgTime = 0;
+		for (int i = 0; i < reads.length; i++)
+			avgTime += times[i];
+		avgTime /= times.length;
+		System.out.println("selectionCompute: " + avgTime);
+		// System.err.println("Running insertion sorts...");
+		for (int i = 0; i < arrays.length; i++) {
+			// System.err.println("Insertion sort " + i + "...");
+			ProfiledList<Integer> result = insertion(arrays[i]);
+			reads[i] = result.reads;
+			writes[i] = result.writes;
+			times[i] = result.elapsed;
+		}
+		avgReads = 0;
+		for (int i = 0; i < reads.length; i++)
+			avgReads += reads[i];
+		avgReads /= reads.length;
+		System.out.println("insertionRead: " + avgReads);
+		avgWrites = 0;
+		for (int i = 0; i < writes.length; i++)
+			avgWrites += writes[i];
+		avgWrites /= writes.length;
+		System.out.println("insertionWrite: " + avgWrites);
+		avgTime = 0;
+		for (int i = 0; i < reads.length; i++)
+			avgTime += times[i];
+		avgTime /= times.length;
+		System.out.println("insertionCompute: " + avgTime);
 	}
 
-	static ProfiledList<Integer> bubble(int length) {
+	static ProfiledList<Integer> bubble(Integer[] array) {
 		ProfiledList<Integer> result = new ProfiledList<Integer>();
-		for (int i = 0; i < length; i++) {
-			result.add((int) Math.floor(Math.random() * result.size()), i);
-		}
-		result.reads = 0; // Clear profiling data for the actual test.
-		result.writes = 0;
+		result.fill(array);
 		result.startTimer();
 		boolean dirty = false;
 		do {
-			for (int i = 0; i < length - 1; i++) {
+			dirty = false;
+			for (int i = 0; i < result.size() - 1; i++) {
 				if (result.get(i) > result.get(i + 1)) {
 					Integer tmp = result.get(i);
 					result.set(i, result.get(i + 1));
 					result.set(i + 1, tmp);
+					dirty = true;
 				}
 			}
 		} while (dirty);
 		result.endTimer();
 		return result;
+	}
+
+	static ProfiledList<Integer> selection(Integer[] array) {
+		ProfiledList<Integer> result = new ProfiledList<Integer>();
+		result.fill(array);
+		result.startTimer();
+		for (int sortedIndices = 0; sortedIndices < result.size(); sortedIndices++) {
+			int minIndex = -1;
+			int minValue = 0;
+			for (int i = sortedIndices; i < result.size(); i++) {
+				int val = result.get(i);
+				if (val < minValue || minIndex == -1) {
+					minValue = val;
+					minIndex = i;
+				}
+			}
+			result.remove(minIndex);
+			result.add(sortedIndices, minValue);
+		}
+		result.endTimer();
+		return result;
+	}
+
+	static ProfiledList<Integer> insertion(Integer[] array) {
+		ProfiledList<Integer> result = new ProfiledList<Integer>();
+		result.fill(array);
+		result.startTimer();
+		for (int i = 1; i < result.size(); ++i) {
+			int val = result.get(i);
+			if (val < result.get(i - 1)) { // This element isn't sorted
+				int tgt = i - 1;
+				for (; tgt > 0; tgt--)
+					if (result.get(tgt - 1) <= val && result.get(tgt) >= val)
+						break;
+				result.remove(i);
+				result.add(tgt, val);
+			}
+		}
+		result.endTimer();
+		return result;
+	}
+
+	static int isSorted(ProfiledList<Integer> list) {
+		int reads = list.reads;
+		int writes = list.writes;
+		for (int i = 1; i < list.size() - 1; ++i) {
+			if (list.get(i) < list.get(i - 1) || list.get(i) > list.get(i + 1)) {
+				return i;
+			}
+		}
+		list.reads = reads;
+		list.writes = writes;
+		return -1;
 	}
 }
